9be10580388dba35f7c1c7c9c578593ac931e8dd
mh-azam <mh.noorazam@gmail.com>
2025-04-21 17:24:31 +0800

    Text, View, Image Components

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
+import Logo from "../assets/img/book-light.png";
 
 const Home = () => {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.img} />
+      {/*<Text style={[styles.title, { color: "purple" }]}>The Number 1</Text>*/}
+      <Text style={styles.title}>The Number 1</Text>
+      <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
+      <View style={styles.card}>
+        <Text>Hello this is a card</Text>
+      </View>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#e0dfe8",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  img: {
+    marginVertical: 20,
+    width: 100,
+    height: 100,
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 5,
+    boxShadow: "4px 4px rgba(0,0,0,0.1)",
+  },
+});
