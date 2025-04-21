46fcb5bad5033d9d8e320d7e5ff9bc28699fb106
mh-azam <mh.noorazam@gmail.com>
2025-04-22 04:14:18 +0800

    lesson 3 - basic file navigation

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 import Logo from "../assets/img/book-light.png";
+import { Link } from "expo-router";
 
 const Home = () => {
   return (
@@ -9,9 +10,13 @@ const Home = () => {
       {/*<Text style={[styles.title, { color: "purple" }]}>The Number 1</Text>*/}
       <Text style={styles.title}>The Number 1</Text>
       <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
-      <View style={styles.card}>
-        <Text>Hello this is a card</Text>
-      </View>
+
+      <Link href="/about" style={styles.link}>
+        About Page
+      </Link>
+      <Link href="/contact" style={styles.link}>
+        Contact Page
+      </Link>
     </View>
   );
 };
@@ -34,10 +39,9 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
   },
-  card: {
-    backgroundColor: "#fff",
-    padding: 20,
-    borderRadius: 5,
-    boxShadow: "4px 4px rgba(0,0,0,0.1)",
+
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1,
   },
 });
