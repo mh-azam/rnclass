c3060be89fdf2c4cf0d3b49e1bdf251afd6ccdc3
mh-azam <mh.noorazam@gmail.com>
2025-04-22 07:14:18 +0800

    lesson 5 - dark and light theme

--- app/about.jsx
+++ app/about.jsx
@@ -1,7 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, useColorScheme } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
+import { Colors } from "../constants/colors";
+
 const About = () => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light;
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>About Page</Text>
