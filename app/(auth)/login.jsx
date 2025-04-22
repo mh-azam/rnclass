5ec740c9f57d9d9458862db9bfa6afa7180921be
mh-azam <mh.noorazam@gmail.com>
2025-04-22 18:32:27 +0800

    lesson8 - Pressable Components

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,18 +1,26 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Pressable, Text } from "react-native";
 import { Link } from "expo-router";
+import { Colors } from "../../constants/colors";
 
+import ThemedButton from "../../components/ThemedButton";
 import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
 import Spacer from "../../components/Spacer";
 
 const Login = () => {
+  const handleSubmit = () => {
+    console.log("Login button pressed");
+  };
+
   return (
     <ThemedView style={styles.container}>
       <Spacer />
       <ThemedText title={true} style={styles.title}>
         Login to Your Account
       </ThemedText>
-
+      <ThemedButton onPress={handleSubmit}>
+        <Text style={{ color: "#f2f2f2", textAlign: "center" }}>Login</Text>
+      </ThemedButton>
       <Spacer height={100} />
       <Link href="/register" replace>
         <ThemedText style={{ textAlign: "center" }}>
@@ -29,10 +37,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
   },
   title: {
     textAlign: "center",
     fontSize: 18,
     marginBottom: 30,
+  },
+  btn: {
+    backgroundColor: Colors.primary,
+    padding: 15,
+    borderRadius: 5,
+  },
+  pressed: {
+    opacity: 0.75,
   },
 });
--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,18 +1,24 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text } from "react-native";
+import { Colors } from "../../constants/colors";
 import { Link } from "expo-router";
-
+import ThemedButton from "../../components/ThemedButton";
 import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
 import Spacer from "../../components/Spacer";
 
 const Register = () => {
+  const handleSubmit = () => {
+    console.log("Register button pressed");
+  };
   return (
     <ThemedView style={styles.container}>
       <Spacer />
       <ThemedText title={true} style={styles.title}>
         Register an Account
       </ThemedText>
-
+      <ThemedButton onPress={handleSubmit}>
+        <Text style={{ color: "#f2f2f2", textAlign: "center" }}>Register</Text>
+      </ThemedButton>
       <Spacer height={100} />
       <Link href="/login" replace>
         <ThemedText style={{ textAlign: "center" }}>Login instead</ThemedText>
@@ -27,10 +33,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
   },
   title: {
     textAlign: "center",
     fontSize: 18,
     marginBottom: 30,
+  },
+  btn: {
+    backgroundColor: Colors.primary,
+    padding: 15,
+    borderRadius: 5,
+  },
+  pressed: {
+    opacity: 0.8,
   },
 });
